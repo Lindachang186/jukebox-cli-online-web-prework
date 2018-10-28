91f7c3f21076616fce463392ade32ab8939f7589
Linda Chang <lindachang186@gmail.com>
2018-10-28 14:24:22 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,8 +11,7 @@ songs = [
 ]
 
 def help
-  puts
-  "I accept the following commands:
+  puts "I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
